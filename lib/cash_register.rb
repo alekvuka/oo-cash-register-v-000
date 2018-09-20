749f11fa6ae6ed4c5a9d23d109ef2f452463b52b
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-20 21:31:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,13 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    
+
   end
 
   def add_item(name, price, quatity = 1)
+    @name = name 
+    @total = @total + price*quantity 
+  end 
 
 
 end
